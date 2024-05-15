7ce5cbb48e7654b18d3996d00e39dba4b7ddf4f6
Dojay6103 <jhy1163@163.com>
2024-05-15 09:40:46 +0800

    upload new

---
+++ model/download.sh
@@ -0,0 +1,7 @@
+git lfs install
+
+git clone https://huggingface.co/Qwen/Qwen1.5-7B
+
+git clone https://huggingface.co/meta-llama/Llama-2-7b-chat-hf  # First you need to apply for use and be authorized
+
+git clone https://huggingface.co/sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2
